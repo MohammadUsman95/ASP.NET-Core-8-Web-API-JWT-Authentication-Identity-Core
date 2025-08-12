5f9152465d4732ee47fabb91d2071d04d64da951
Mohammad Usman <144525606+MohammadUsman95@users.noreply.github.com>
2025-08-12 23:28:42 +0500

    Add files via upload

---
+++ AuthorizationDemoEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AuthECAPI.Controllers
+{
+    public static class AuthorizationDemoEndpoints
+    {
+        public static IEndpointRouteBuilder MapAuthorizationDemoEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/AdminOnly", AdminOnly);
+            app.MapGet("/AdminOrTeacher", [Authorize(Roles = "Admin,Teacher")] () =>
+            {
+                return "Admin or Teacher";
+            });
+
+            app.MapGet("/LibraryMembersOnly", [Authorize(Policy = "HaSLibraryId")] () =>
+            {
+                return "Library Members Only";
+            });
+            app.MapGet("/ApplyForMaternityLeave", [Authorize(Roles = "Teacher", Policy = "FemalesOnly")] () =>
+            {
+                return "Applied for maternity leave";
+            });
+
+            return app;          
+        }
+
+        [Authorize(Roles = "Admin")]
+        private static string AdminOnly() 
+        {
+            return "Admin Only";
+        }
+    }
+}
